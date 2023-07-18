08b7bbfbe55b0d4b128825b63b7adad1ee77f019
Einav Koren <einavk@wix.com>
2023-07-18 15:02:12 +0300

    adding ejs templating engine

--- my-node-app/app.js
+++ my-node-app/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -13,6 +16,8 @@ const usersRoutes = require('./routes/users');
 app.use(homeRoutes);
 app.use(usersRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: "" });
+  });
 
 app.listen(3000);
-;
--- my-node-app/routes/users.js
+++ my-node-app/routes/users.js
@@ -1,12 +1,16 @@
-const path = require('path');
 const express = require('express');
-
-const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const users = [];
+
 router.get('/users', (req,res,next) => {
-    res.sendFile(path.join(rootDir, 'views', 'users.html'))
+    res.render('users', {pageTitle: 'Users', path: '/users', users: users} );
 });
+
+router.post('/users', (req, res, next) => {
+    users.push({ title: req.body.title });
+    res.redirect('/');
+})
 
 module.exports = router;
