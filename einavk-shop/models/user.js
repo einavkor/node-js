d658fbbc52d4869a04521688ca43cdbb40cd5a91
Einav Koren <einavk@wix.com>
2023-07-20 19:46:53 +0300

    finish

--- einavk-shop/models/user.js
+++ einavk-shop/models/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
@@ -95,6 +96,38 @@ class User {
         { _id: new ObjectId(this._id) },
         { $set: { cart: { items: updatedCartItems } } }
       );
+  }
+
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection('orders').insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
+        db.collection('users').updateOne(
+          { _id: new ObjectId(this._id) },
+          { $set: { cart: { items: [] } } }
+        );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({
+        'user._id': new ObjectId(this._id),
+      })
+      .toArray();
   }
 
   static findById(userId) {
